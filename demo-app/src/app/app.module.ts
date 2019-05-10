51bfb0c166552c4bdd760bec53917a285ecefff8
pradeep <pradeep.kumar44@gmail.com>
2019-05-10 15:08:48 +0530

    committing the changes

--- demo-app/src/app/app.module.ts
+++ demo-app/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainComponent } from './main/main.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     HeaderComponent,
     SidebarComponent,
     FooterComponent,
-    NavBarComponent
+    NavBarComponent,
+    MainComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
